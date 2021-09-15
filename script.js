c1a62484c595d2aa4dcc17f9e5fd06620556e032
BELHASS Mohamed Adil <72229076+Adil-production@users.noreply.github.com>
2021-09-15 19:38:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 
-   
+   window.addEventListener('load', function () {
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("all").style.display = "block";
+    document.getElementById("nav").style.display = "block";
+
+  })
+
     var API = 'https://api.nasa.gov/planetary/apod?api_key=5LEPXoSG69ti0yLNcsvA2gKjt4V5oxnlzak5Vey0';
     
     
@@ -10,22 +16,30 @@
         const data = await res.json()
        
         console.log(data)
-        document.querySelector("#main #card #card-body #title").innerHTML = data.title;
-        document.querySelector("#main #card #img").src = data.url;
-        document.querySelector("#main #card #publish").innerHTML = data.date;
-        document.querySelector("#img2").src = data.url;
-       document.querySelector("#article #exp").innerHTML = data.explanation;
+        document.querySelector("#all #main #card #card-body #title").innerHTML = data.title;
+        document.querySelector("#all #main #card #img").src = data.url;
+        document.querySelector("#all #main #card #publish").innerHTML = data.date;
+        document.querySelector("#all #img2").src = data.url;
+       document.querySelector("#all #article #exp").innerHTML = data.explanation;
     }
     function exp(){
-        document.querySelector("#main").style.display = "none";
+        document.querySelector("#all #main").style.display = "none";
         document.getElementById("article").style.display = "block";
         document.getElementById("back").style.display = "inline-block";
     }
     function back(){
-        document.querySelector("#main").style.display = "block";
+        document.querySelector("#all #main").style.display = "block";
         document.getElementById("article").style.display = "none";
         document.getElementById("back").style.display = "none";
     }
+function loading(){
+    if(document.getElementById("img").naturalHeight){
+        document.getElementById("loading").style.display = "none";
+    }
+    else{
+        document.getElementById("loading").style.display = "block";
+    }
+}
   
     
    
